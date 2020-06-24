c6f2094ea661f8593b1ce95fc03bdee26e55f534
watgrad <watgrad@gmail.com>
2020-06-23 21:50:18 -0400

    First Commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login' # same as the name you would use for url_for() calls
+
+from app import routes, models
+
+
